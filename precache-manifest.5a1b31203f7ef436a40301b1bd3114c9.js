e0948850d2bd7f528fc25374d6b3cd99de8fe919
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2020-09-11 21:01:07 -0300

    Updates

---
+++ precache-manifest.5a1b31203f7ef436a40301b1bd3114c9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455e6f2450431bdb0114bc6bb49abac8",
+    "url": "/newsfeed-react-redux/index.html"
+  },
+  {
+    "revision": "4d19543b9a824dca5c70",
+    "url": "/newsfeed-react-redux/static/css/main.24720585.chunk.css"
+  },
+  {
+    "revision": "9f2bb02b82afefb05045",
+    "url": "/newsfeed-react-redux/static/js/2.632d12bc.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/newsfeed-react-redux/static/js/2.632d12bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d19543b9a824dca5c70",
+    "url": "/newsfeed-react-redux/static/js/main.2892edbb.chunk.js"
+  },
+  {
+    "revision": "1ddfb36760ad1ed0f16e",
+    "url": "/newsfeed-react-redux/static/js/runtime-main.d00f4553.js"
+  },
+  {
+    "revision": "40000a14460bf333e592b0641e83b6f5",
+    "url": "/newsfeed-react-redux/static/media/icon_cross.40000a14.svg"
+  },
+  {
+    "revision": "c99d4589eef2eb3c8042dbea8d3cd5cb",
+    "url": "/newsfeed-react-redux/static/media/icon_home.c99d4589.svg"
+  },
+  {
+    "revision": "eda0d27e17ff5850570afeb2b2b2be83",
+    "url": "/newsfeed-react-redux/static/media/icon_search.eda0d27e.svg"
+  },
+  {
+    "revision": "5389d7de8f1c31aae167e7199497f504",
+    "url": "/newsfeed-react-redux/static/media/icon_sorry.5389d7de.svg"
+  },
+  {
+    "revision": "7dc058f51e01def526cebf2bb2bf6637",
+    "url": "/newsfeed-react-redux/static/media/icon_up.7dc058f5.svg"
+  },
+  {
+    "revision": "d6cd5151c04765d1992edfde14483068",
+    "url": "/newsfeed-react-redux/static/media/image_loading.d6cd5151.gif"
+  },
+  {
+    "revision": "f6ceb17f085dd8db3003f3037331ec5e",
+    "url": "/newsfeed-react-redux/static/media/logo_news.f6ceb17f.svg"
+  },
+  {
+    "revision": "f7ed6d1157401092ba81d160309b6748",
+    "url": "/newsfeed-react-redux/static/media/placeholder_news.f7ed6d11.svg"
+  }
+]);
